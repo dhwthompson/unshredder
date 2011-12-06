03f8865d4732c2ae5e8e97f18f2e00a5e44284ca
David Thompson <mail@fatbusinessman.com>
2011-12-06 23:05:04 +0000

    Find the best match and reconstitute.
    Ok, so it doesn't quite reconstitute it *correctly*, but the principle
    is sound. The heuristic is just off.

--- unshredder.py
+++ unshredder.py
@@ -2,12 +2,13 @@ from __future__ import print_function
 
 import sys
 import logging
-from itertools import product
+from itertools import izip, permutations, product
 
 from PIL import Image
 
 LOGGER = logging.getLogger(__name__)
 COLUMN_WIDTH = 32
+GOOD_MATCH_THRESHOLD = 0.4
 
 
 def get_columns(input_image):
@@ -37,13 +38,22 @@ def column_difference(left, right):
     left_slice = left.crop((left.size[0] - 1, 0, left.size[0], left.size[1]))
     right_slice = right.crop((0, 0, 1, right.size[1]))
     
-    return sum(pixel_difference(a, b) for a, b in
-               zip(left_slice.getdata(), right_slice.getdata()))
+    left_slice, right_slice = (s.getdata() for s in [left_slice, right_slice])
+    
+    differences = []
+    # Find the adjoining pixel of closest value and use that
+    for i, left_pixel in enumerate(left_slice):
+        # Pixel data objects don't support slicing, so have to do it this way
+        pixel_range = range(max(0, i - 1), min(len(right_slice), i + 2))
+        differences.append(min(pixel_difference(left_pixel, right_slice[r])
+                               for r in pixel_range))
+    
+    return sum(differences)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: %s inputfile' % sys.argv[0], file=sys.stderr)
+    if len(sys.argv) != 3:
+        print('Usage: %s infile outfile' % sys.argv[0], file=sys.stderr)
         sys.exit(1)
     
     input_image = Image.open(sys.argv[1])
@@ -55,13 +65,54 @@ if __name__ == '__main__':
     
     diffs = {}
     for (a, col_a), (b, col_b) in product(enumerate(columns), enumerate(columns)):
+        if a == b:
+            continue
         diffs[a, b] = column_difference(col_a, col_b)
     
-    print(diffs)
     print('Min: %d' % min(diffs.values()))
     median = sorted(diffs.values())[len(diffs) // 2]
     print('Median: %d' % median)
     print('Max: %d' % max(diffs.values()))
     
-    print(list(pair for pair, value in diffs.items()
-               if float(value) / median < 0.4))
+    good_matches = set([pair for pair, value in diffs.items()
+                       if float(value) / median < GOOD_MATCH_THRESHOLD])
+    
+    chains = []
+    while good_matches:
+        new_item = good_matches.pop()
+        before = [x for x in chains if x[-1] == new_item[0]]
+        after = [x for x in chains if x[0] == new_item[-1]]
+        if len(before) > 2 or len(after) > 2:
+            raise RuntimeError('Too many obvious matches')
+        if before:
+            chains.remove(before[0])
+            new_item = before[0][:-1] + new_item
+        if after:
+            chains.remove(after[0])
+            new_item = new_item + after[0][1:]
+        chains.append(new_item)
+    
+    # Fill in any columns that aren't covered
+    chained_columns = set(sum(chains, ()))
+    chains = chains + [(i,) for i in range(len(columns))
+                       if i not in chained_columns]
+    print(chains)
+    best = None
+    for i, permutation in enumerate(permutations(chains)):
+        print(i, end='\r')
+        cost = 0
+        for (a, b) in izip(permutation, permutation[1:]):
+            cost += diffs[a[-1], b[0]]
+        if best is None or best[1] > cost:
+            best = permutation, cost
+    print()
+    
+    print(best[0])
+    ordering = sum(best[0], ())
+    
+    image_out = Image.new(input_image.mode, input_image.size)
+    
+    for i, column in enumerate(columns[x] for x in ordering):
+        image_out.paste(column, (COLUMN_WIDTH * i, 0))
+    
+    image_out.save(open(sys.argv[2], mode='w'))
